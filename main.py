97cc200a3901bd55d664629b0878846db02a3757
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2024-07-26 10:59:05 -0300

    rota de pesquisas da api

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import uvicorn
 
 app = fastapi.FastAPI()
 
+@app.get("/dados_produtos_destaque")
+def dados_produtos_destaque():
+    dados_produtos_destaque = Banco_Produtos_Loja_Esportiva()
+
+    return dados_produtos_destaque.Ver_Dados_Produtos_Destaque()
+
 @app.get("/dados_produtos")
 def ver_dados_produtos():
     dados_produtos = Banco_Produtos_Loja_Esportiva()
@@ -19,7 +25,23 @@ def adicionar_produto_api(nome_produto:str, preco_produto:float, imagem_produto:
                                      imagem_produto=imagem_produto,
                                      id_produto=id_produto)
     
-    return {"Adicionado": True}
+    return "Produto Adicionado"
+
+@app.get("/categoria_produtos/{categoria}")
+def filtra_produtos(categoria:str):
+    filtro_produtos = Banco_Produtos_Loja_Esportiva()
+
+    produtos_filtrados = filtro_produtos.Filtrar_Dados_Produtos(categoria=categoria)
+
+    return produtos_filtrados
+
+@app.get("/pesquisa_produtos/{produto}")
+def pesquisa_produtos(produto:str):
+    pesquisa_produto = Banco_Produtos_Loja_Esportiva()
+
+    produtos_pesquisados = pesquisa_produto.Pesquisar_Dados_Produtos(nome_produto=produto)
+
+    return produtos_pesquisados
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info", reload=True)
